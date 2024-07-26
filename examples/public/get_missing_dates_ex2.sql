3f22f5088023cc332eed0c34a95103c17d0d20b1
Shankar Arul <shankar@jupyterdata.com>
2024-07-26 13:02:41 +0200

    beefed up documentation

---
+++ examples/public/get_missing_dates_ex2.sql
@@ -0,0 +1,12 @@
+/**
+ * Retrieves the missing dates in the date_month column of the missing_month model at the Monthly level granularity.
+ *
+ * @param {string} missing_day - The name of the model.
+ * @param {string} date_day - The name of the column containing the dates.
+ * @param {array} [dimensions] - An optional array of dimensions across which the missing dates are computed.
+ * @param {object} [filters] - An optional filter object.
+ * @param {string} [expected_frequency] - The interval to use when generating the missing dates (e.g. 'DAY', 'MONTH', 'YEAR').
+ * @returns {rows} Rows of missing dates.
+ */
+
+{{get_missing_date('missing_month','date_month', ['country'], {}, 'MONTH')}}
